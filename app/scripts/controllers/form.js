d269b2fceca593916036e73acb4979ad499c7241
Benjamin Orozco <benoror@gmail.com>
2015-03-30 18:13:18 -0600

    schema-form: Form Actions placeholders

--- app/scripts/controllers/form.js
+++ app/scripts/controllers/form.js
@@ -37,10 +37,47 @@ angular.module('panaxuiApp')
 			CRUDService.read(params).then(function (res) {
 				$scope.schema = res.data.schema;
 				$scope.form = res.data.form || ['*'];
+				$scope.form.push({
+					type: "actions",
+					style: "pull-right",
+					items: [
+				    { 
+				    	type: 'submit', 
+				    	title: 'Save',
+				    	style: 'btn-primary', 
+				    	icon: 'glyphicon glyphicon-ok-sign'
+				    }, { 
+				    	type: 'button', 
+				    	title: 'Cancel', 
+				    	onClick: "onCancel()",
+				    	style: 'btn-default', 
+				    	icon: 'glyphicon glyphicon-ban-circle'
+				    }
+					]
+				});
 				$scope.model = res.data.model[0] || {};
 			});
 		};
 		$scope.loadSchemaForm();
+
+		// Submit handler
+		$scope.onSubmit = function(form) {
+			// First we broadcast an event so all fields validate themselves
+			$scope.$broadcast('schemaFormValidate');
+
+			// Then we check if the form is valid
+			if (form.$valid) {
+				// ... do whatever you need to do with your data.
+				console.log('FORM VALID')
+			} else {
+				console.log('INVALID FORM NOT GOOD')
+			}
+		}
+
+		// Cancel handler
+		$scope.onCancel = function() {
+			// ToDo: Alert of unsaved dirty form otherwise/then Go Back
+		}
 
 		// Reload listener
 		$scope.$on('reloadData', function (event, next) {
