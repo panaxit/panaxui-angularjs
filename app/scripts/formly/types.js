ad80ca64198c57d2ceb704892908d969d39f4c49
Benjamin Orozco <benoror@gmail.com>
2015-05-15 13:29:59 -0500

    angular-formly: More HTML5 input types: datetime, date, time (ToDo: Implement AngularUI date/time picker)

--- app/scripts/formly/types.js
+++ app/scripts/formly/types.js
@@ -94,34 +94,10 @@ angular.module('panaxuiApp')
     });
     /**
      * Date/Time controls
-     * ToDo: Use AngularUI alternatives?
+     * ToDo: Use AngularUI alternatives? (some HTML5 input types not tupported by IE)
      * 	- http://angular-ui.github.io/bootstrap/#/datepicker
      * 	- angular-ui.github.io/bootstrap/#/timepicker
      */
-    /*
-	    date
-     */
-    formlyConfigProvider.setType({
-      name: 'date',
-      extends: 'input',
-      defaultOptions: {
-      	templateOptions: {
-      		type: 'date'
-      	}
-      }
-    });
-    /*
-	    time
-     */
-    formlyConfigProvider.setType({
-      name: 'time',
-      extends: 'input',
-      defaultOptions: {
-      	templateOptions: {
-      		type: 'time'
-      	}
-      }
-    });
     /*
 	    datetime
      */
@@ -132,6 +108,72 @@ angular.module('panaxuiApp')
       	templateOptions: {
       		type: 'datetime-local'
       	}
+      },
+      controller: function ($scope) {
+      	/**
+      	 * HTML5 input type=date
+      	 * Convert date string to Date object
+      	 */
+      	var newValue = (function dateConversion(oldValue) {
+      		if(oldValue) {
+      			return new Date(oldValue);
+      		}
+      		return undefined;
+      	})($scope.model[$scope.options.key]);
+
+      	// Change in model
+      	$scope.model[$scope.options.key] = newValue;
+      	// Change original value as well (keep in $pristine)
+      	$scope.options.value = newValue;
+      }
+    });
+    /*
+	    date
+	    extends: datetime
+     */
+    formlyConfigProvider.setType({
+      name: 'date',
+      extends: 'datetime',
+      defaultOptions: {
+      	templateOptions: {
+      		type: 'date'
+      	}
+      }
+    });
+    /*
+	    time
+	    extends: datetime
+     */
+    formlyConfigProvider.setType({
+      name: 'time',
+      extends: 'input',
+      defaultOptions: {
+      	templateOptions: {
+      		type: 'time'
+      	}
+      },
+      controller: function ($scope) {
+      	/**
+      	 * HTML5 input type=time
+      	 * Convert time string to Date object
+      	 */
+      	var newValue = (function timeConversion(oldValue) {
+      		if(oldValue) {
+	      		// John Resig's: 
+	      		// http://stackoverflow.com/questions/141348/what-is-the-best-way-to-parse-a-time-into-a-date-object-from-user-input-in-javas
+	      		var d = new Date();
+	      		var time = oldValue.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+	      		d.setHours( parseInt(time[1]) + (time[3] ? 12 : 0) );
+	      		d.setMinutes( parseInt(time[2]) || 0 );
+	      		return d;
+      		}
+      		return undefined;
+      	})($scope.model[$scope.options.key]);
+
+      	// Change in model
+      	$scope.model[$scope.options.key] = newValue;
+      	// Change original value as well (keep in $pristine)
+      	$scope.options.value = newValue;
       }
     });
     /*
