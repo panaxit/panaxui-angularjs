ba138009f62ee1a9955d9cea59d4b25ec762c092
Benjamin Orozco <benoror@gmail.com>
2015-05-18 10:54:00 -0500

    debug window: environment section added (versions, global variables, etc)

--- app/scripts/controllers/debug.js
+++ app/scripts/controllers/debug.js
@@ -8,10 +8,15 @@
  * Controller of the panaxuiApp
  */
 angular.module('panaxuiApp')
-	.controller('DebugCtrl', function($scope, $modalInstance, debugInfo) {
+	.controller('DebugCtrl', function($scope, $modalInstance, formlyVersion, debugInfo) {
 
 		// Set debugInfo provider
 		$scope.debugInfo = debugInfo;
+
+		$scope.debugInfo.env = {
+      angularVersion: angular.version.full,
+      formlyVersion: formlyVersion
+		}
 
 		// Ok clicked
 		$scope.okClick = function() {
