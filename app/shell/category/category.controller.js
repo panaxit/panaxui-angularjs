ef53546f20e2932c8d8c2e08f7505ea063689493
Benjamin Orozco <benoror@gmail.com>
2015-08-04 19:32:26 -0500

    fix(category): ng-repeat rows in chunks of thumbnails

--- app/shell/category/category.controller.js
+++ app/shell/category/category.controller.js
@@ -2,9 +2,13 @@ export default class CategoryCtrl {
   constructor($rootScope, $scope, DebugService) {
     var vm = this;
 
-    vm.currentNavBranch = $rootScope.currentNavBranch;
+    vm.categoryTitle = $rootScope.currentNavBranch.label;
+    $scope.$emit('setPanelTitle', 'Category: ' + vm.categoryTitle);
 
-    $scope.$emit('setPanelTitle', 'Category: ' + vm.currentNavBranch.label);
+    vm.categoryChildren = [];
+    var chunk = 5;
+    for(var i=0;i < $rootScope.currentNavBranch.children.length; i+=chunk)
+      vm.categoryChildren.push($rootScope.currentNavBranch.children.slice(i, i+chunk));
 
     vm.onClick = function(node) {
       $scope.$emit('goToBranch', node);
