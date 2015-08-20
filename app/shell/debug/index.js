10eec9305b3e651790eaba291f283dd9327c808f
Benjamin Orozco <benoror@gmail.com>
2015-08-20 11:23:50 -0500

    feat(debug,package.json): add jsonFormatter to debug view

--- app/shell/debug/index.js
+++ app/shell/debug/index.js
@@ -3,8 +3,12 @@ import angular from 'angular';
 import DebugService from './debug.service';
 import DebugCtrl from './debug.controller';
 
+import jsonFormatter from 'jsonFormatter';
+import 'jsonFormatter/dist/json-formatter.css';
+
 export default angular.module('app.debug', [
-    DebugService
+    DebugService,
+    'jsonFormatter'
   ])
   .controller('DebugCtrl', DebugCtrl)
   .name;
