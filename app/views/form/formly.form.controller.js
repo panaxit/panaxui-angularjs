2fa4359dac7c1037f90438810f4ebede6984d156
Benjamin Orozco <benoror@gmail.com>
2015-08-20 14:06:53 -0500

    fix(formly[form]): ugly fix to allow paginated nested forms when single-record (object) and multi-record (array)

--- app/views/form/formly.form.controller.js
+++ app/views/form/formly.form.controller.js
@@ -20,6 +20,9 @@ export default class FormlyFormCtrl extends FormCtrl {
     var vm = this;
 
     vm.data = vm.$scope.model[vm.$scope.options.key] || [];
+    if(!angular.isArray(vm.data)) { // If single record
+      vm.data = [vm.data];
+    }
     vm.fields = vm.$scope.options.data.fields;
     vm.catalog = vm.$scope.options.data.catalog;
   }
