e151ca86e351ec1ff45d556c5dff40e6b107bf74
Benjamin Orozco <benoror@gmail.com>
2015-09-10 18:12:38 -0500

    refactor(main) reorder loading components/directives in index

--- app/shell/main/index.js
+++ app/shell/main/index.js
@@ -6,19 +6,23 @@ import ngUrlify from 'angular-urlify';
 import ngTreeControl from 'angular-tree-control';
 import 'angular-tree-control/css/tree-control.css';
 
+// Shell Components
 import MainCRUD from '../../shell/crud';
 import MainDebug from '../../shell/debug';
 import MainHome from '../../shell/home';
 import MainCategory from '../../shell/category';
 import MainPanel from '../../shell/panel';
 
+// Panax Directives
+import pxGrid from '../../directives/px-grid';
+import pxForm from '../../directives/px-form';
+import pxCards from '../../directives/px-cards';
+
+// Data Components
 import GridView from '../../views/grid';
 import FormView from '../../views/form';
 import CardsView from '../../views/cards';
 import MasterDetail from '../../views/master-detail';
-import pxGrid from '../../directives/px-grid';
-import pxForm from '../../directives/px-form';
-import pxCards from '../../directives/px-cards';
 
 import routing from './main.routes';
 import MainCtrl from './main.controller';
@@ -34,13 +38,14 @@ export default angular.module('app.main', [
     MainCategory,
     MainPanel,
 
+    pxGrid,
+    pxForm,
+    pxCards,
+
     GridView,
     FormView,
     CardsView,
-    MasterDetail,
-    pxGrid,
-    pxForm,
-    pxCards
+    MasterDetail
 	])
 	.config(routing)
 	.controller('MainCtrl', MainCtrl)
