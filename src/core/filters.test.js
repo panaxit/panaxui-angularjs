0617912b1423dba3f24fddba7f07ff11c2ff5e69
Benjamin Orozco <benoror@gmail.com>
2015-09-22 16:50:13 -0500

    feat(tests): filters

---
+++ src/core/filters.test.js
@@ -0,0 +1,32 @@
+import module from './filters';
+
+describe('Filters', () => {
+
+  beforeEach(angular.mock.module(module));
+
+  describe('Min', () => {
+    var minFilter;
+
+    beforeEach(angular.mock.inject(function(_minFilter_) {
+      minFilter = _minFilter_;
+    }));
+
+    it('should get Infinity from nothing', () => {
+      var values;
+      var min = minFilter(values);
+      expect(min).to.equal(Infinity);
+
+      values = [];
+      min = minFilter(values);
+      expect(min).to.equal(Infinity);
+    });
+
+    it('should get the min value from array', () => {
+      var values = [0, 10, -1, 56];
+      var min = minFilter(values);
+      expect(min).to.equal(-1);
+    });
+
+  });
+
+});
