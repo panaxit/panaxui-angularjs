62d324f9da8732718af9330a1d7195a0cf638d52
Benjamin Orozco <benoror@gmail.com>
2015-09-22 17:07:22 -0500

    feat(tests): shell's services placeholders

---
+++ src/shell/alert/alert.service.test.js
@@ -0,0 +1,14 @@
+import module from './alert.service';
+
+describe('Service: Alert', () => {
+  var AlertService;
+
+  beforeEach(angular.mock.module(module));
+
+  beforeEach(angular.mock.inject(function(_AlertService_) {
+    AlertService = _AlertService_;
+  }));
+
+  it('PENDING TESTS');
+
+});
---
+++ src/shell/debug/debug.service.test.js
@@ -0,0 +1,14 @@
+import module from './debug.service';
+
+describe('Service: Debug', () => {
+  var DebugService;
+
+  beforeEach(angular.mock.module(module));
+
+  beforeEach(angular.mock.inject(function(_DebugService_) {
+    DebugService = _DebugService_;
+  }));
+
+  it('PENDING TESTS');
+
+});
