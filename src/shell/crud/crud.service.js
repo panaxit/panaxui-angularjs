e9f9d37d63607ca86c4c01ffd8ba85d3b2160096
Benjamin Orozco <benoror@gmail.com>
2015-09-24 18:05:14 -0500

    style(src[crud]): class style

--- src/shell/crud/crud.service.js
+++ src/shell/crud/crud.service.js
@@ -3,112 +3,119 @@ import angular from 'angular';
 class CRUDService {
   constructor($http, $q) {
     var vm = this;
-
-    /*
-    Set base URL if needed (ex. when testing)
-     */
+    vm.$http = $http;
+    vm.$q = $q;
     vm.base_url = '';
+  }
 
-    vm.setBaseURL = function(url) {
-      vm.base_url = url;
-    };
+  /*
+  Set base URL if needed (ex. when testing)
+   */
+  setBaseURL(url) {
+    var vm = this;
+    vm.base_url = url;
+  }
 
-    /**
-     * GET /api/read
-     */
-    vm.read = function(params) {
-      var deferred = $q.defer();
-      params.gui = 'ng';
-      params.output = 'json';
+  /**
+   * GET /api/read
+   */
+  read(params) {
+    var vm = this;
+    var deferred = vm.$q.defer();
+    params.gui = 'ng';
+    params.output = 'json';
 
-      $http
-        .get(vm.base_url + "/api/read", {params: params})
-        .then(function (response) {
-          deferred.resolve(response.data);
-        });
-      return deferred.promise;
-    };
+    vm.$http
+      .get(vm.base_url + "/api/read", {params: params})
+      .then(function (response) {
+        deferred.resolve(response.data);
+      });
+    return deferred.promise;
+  }
 
-    /**
-     * GET /api/options
-     */
-    vm.options = function(params) {
-      var deferred = $q.defer();
-      params.gui = 'ng';
-      params.array = true;
+  /**
+   * GET /api/options
+   */
+  options(params) {
+    var vm = this;
+    var deferred = vm.$q.defer();
+    params.gui = 'ng';
+    params.array = true;
 
-      if(params.foreignEntity && params.foreignKey && params.foreignValue)
-        params.filters = "["+params.foreignKey+"='"+params.foreignValue+"']";
+    if(params.foreignEntity && params.foreignKey && params.foreignValue)
+      params.filters = "["+params.foreignKey+"='"+params.foreignValue+"']";
 
-      $http
-        .get(vm.base_url + "/api/options", {params: params})
-        .then(function (response) {
-          deferred.resolve(response.data);
-        });
-      return deferred.promise;
-    };
+    vm.$http
+      .get(vm.base_url + "/api/options", {params: params})
+      .then(function (response) {
+        deferred.resolve(response.data);
+      });
+    return deferred.promise;
+  }
 
-    /**
-     * POST /api/create
-     */
-    vm.create = function(payload) {
-      var deferred = $q.defer();
+  /**
+   * POST /api/create
+   */
+  create(payload) {
+    var vm = this;
+    var deferred = vm.$q.defer();
 
-      $http
-        .post(vm.base_url + "/api/create", payload)
-        .then(function (response) {
-          deferred.resolve(response.data);
-        });
-      return deferred.promise;
-    };
+    vm.$http
+      .post(vm.base_url + "/api/create", payload)
+      .then(function (response) {
+        deferred.resolve(response.data);
+      });
+    return deferred.promise;
+  }
 
-    /**
-     * PUT /api/update
-     */
-    vm.update = function(payload) {
-      var deferred = $q.defer();
+  /**
+   * PUT /api/update
+   */
+  update(payload) {
+    var vm = this;
+    var deferred = vm.$q.defer();
 
-      $http
-        .put(vm.base_url + "/api/update", payload)
-        .then(function (response) {
-          deferred.resolve(response.data);
-        });
-      return deferred.promise;
-    };
+    vm.$http
+      .put(vm.base_url + "/api/update", payload)
+      .then(function (response) {
+        deferred.resolve(response.data);
+      });
+    return deferred.promise;
+  }
 
-    /**
-     * DELETE /api/delete
-     */
-    vm.delete = function(payload) {
-      var deferred = $q.defer();
+  /**
+   * DELETE /api/delete
+   */
+  delete(payload) {
+    var vm = this;
+    var deferred = vm.$q.defer();
 
-      // http://stackoverflow.com/questions/24829933/angularjs-delete-with-data-sets-wrong-content-type-header
-      $http({
-        url: vm.base_url + "/api/delete",
-        method: 'DELETE',
-        data: payload,
-        headers: {'Content-Type': 'application/json'}
-      })
-        .then(function (response) {
-          deferred.resolve(response.data);
-        });
-      return deferred.promise;
-    };
+    // http://stackoverflow.com/questions/24829933/angularjs-delete-with-data-sets-wrong-content-type-header
+    vm.$http({
+      url: vm.base_url + "/api/delete",
+      method: 'DELETE',
+      data: payload,
+      headers: {'Content-Type': 'application/json'}
+    })
+      .then(function (response) {
+        deferred.resolve(response.data);
+      });
+    return deferred.promise;
+  }
 
-    /**
-     * POST /api/filters
-     */
-    vm.filters = function(payload) {
-      var deferred = $q.defer();
+  /**
+   * POST /api/filters
+   */
+  filters(payload) {
+    var vm = this;
+    var deferred = vm.$q.defer();
 
-      $http
-        .post(vm.base_url + "/api/filters", payload)
-        .then(function (response) {
-          deferred.resolve(response.data);
-        });
-      return deferred.promise;
-    };
-
+    vm.$http
+      .post(vm.base_url + "/api/filters", payload)
+      .then(function (response) {
+        deferred.resolve(response.data);
+      });
+    return deferred.promise;
   }
 }
 
