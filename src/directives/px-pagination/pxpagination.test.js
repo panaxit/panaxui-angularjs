aee05d22291e947f8298cea66d4c06624191cd70
Benjamin Orozco <benoror@gmail.com>
2015-09-24 13:21:28 -0500

    test(directives): placeholders for code coverage

---
+++ src/directives/px-pagination/pxpagination.test.js
@@ -0,0 +1,16 @@
+import module from './index';
+
+describe('Directive: pxPagination', () => {
+  var $compile,
+      $rootScope;
+
+  beforeEach(angular.mock.module(module));
+
+  beforeEach(angular.mock.inject(function(_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  it('PENDING');
+
+});
